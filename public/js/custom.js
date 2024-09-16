f8fbe52bbc47920fd54aba14f91b115ff27ee7b7
UUID <69064727+zzy-design@users.noreply.github.com>
2024-09-17 00:48:17 +0800

    Update custom.js

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,4 +1,3 @@
-// 这里编写自定义js脚本；将被静态引入到页面中
 <script src="https://readmore.openwrite.cn/js/readmore-2.0.js" type="text/javascript"></script>
 <script>
     var article = document.getElementsByTagName("article")[0];
@@ -10,7 +9,7 @@
         qrcode: 'https://open.weixin.qq.com/qr/code?username=gh_de98467384ad',
         keyword: 'more',
         btnText: '原创不易，完成人机检测，阅读全文',
-        cookieAge: 1,
+        cookieAge: 10,
         displayPercentage: 0.4
     });
 </script>
